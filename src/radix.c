c5d68ef553e8f038b1470d58818611d31cc1a41c
DEATH <66733557+Deathtaek1020@users.noreply.github.com>
2025-09-22 11:23:08 +0300

    Update src/radix.c with new implementation

--- src/radix.c
+++ src/radix.c
@@ -1,75 +1,16 @@
 #include "push_swap.h"
 
-static void	sort_int_array(int *arr, int n)
-{
-	int	i;
-	int	j;
-	int	t;
-
-	i = 0;
-	while (i < n)
-	{
-		j = i + 1;
-		while (j < n)
-		{
-			if (arr[j] < arr[i])
-			{
-				t = arr[i];
-				arr[i] = arr[j];
-				arr[j] = t;
-			}
-			j++;
-		}
-		i++;
-	}
-}
-
-void	index_values(t_node *a)
-{
-	int	n;
-	int	i;
-	int	*arr;
-	t_node	*cur;
-
-	n = stack_size(a);
-	if (n <= 0)
-		return ;
-	arr = (int *)ft_calloc(n, sizeof(int));
-	if (!arr)
-		return ;
-	cur = a;
-	i = 0;
-	while (cur)
-	{
-		arr[i++] = cur->val;
-		cur = cur->next;
-	}
-	sort_int_array(arr, n);
-	cur = a;
-	while (cur)
-	{
-		i = 0;
-		while (i < n && arr[i] != cur->val)
-			i++;
-		cur->idx = i;
-		cur = cur->next;
-	}
-	free(arr);
-}
-
-static int	max_bits_from_stack(t_node *a)
+static int	get_max_bits(t_node *a)
 {
 	int	max;
 	int	bits;
-	t_node	*cur;
 
 	max = 0;
-	cur = a;
-	while (cur)
+	while (a)
 	{
-		if (cur->idx > max)
-			max = cur->idx;
-		cur = cur->next;
+		if (a->idx > max)
+			max = a->idx;
+		a = a->next;
 	}
 	bits = 0;
 	while ((max >> bits) != 0)
@@ -79,31 +20,63 @@ static int	max_bits_from_stack(t_node *a)
 	return (bits);
 }
 
+void	index_values(t_node *a)
+{
+	t_node	*i;
+	t_node	*j;
+	int	rank;
+
+i = a;
+	while (i)
+	{
+		rank = 0;
+		j = a;
+		while (j)
+		{
+			if (j->val < i->val)
+				rank++;
+			j = j->next;
+		}
+		i->idx = rank;
+		i = i->next;
+	}
+}
+
+static void	radix_pass(t_node **a, t_node **b, int bit, int size)
+{
+	int	i;
+	t_node	*cur;
+
+i = 0;
+	while (i < size)
+	{
+		cur = *a;
+		if (((cur->idx >> bit) & 1) == 1)
+			ra(a);
+		else
+			pb(a, b);
+		i++;
+	}
+	while (*b)
+		pa(a, b);
+}
+
 void	radix_sort(t_node **a, t_node **b)
 {
+	int	size;
 	int	bits;
-	int	sz;
-	int	i;
-	int	j;
+	int	b;
 
 	if (!a || !*a)
 		return ;
-	bits = max_bits_from_stack(*a);
-	sz = stack_size(*a);
-	i = 0;
-	while (i < bits)
+	size = stack_size(*a);
+	if (size <= 1 || stack_is_sorted_vals(*a))
+		return ;
+	bits = get_max_bits(*a);
+	b = 0;
+	while (b < bits)
 	{
-		j = 0;
-		while (j < sz)
-		{
-			if (((*a)->idx >> i) & 1)
-				ra(a);
-			else
-				pb(a, b);
-			j++;
-		}
-		while (*b)
-			pa(a, b);
-		i++;
+		radix_pass(a, b, b, size);
+		b++;
 	}
 }
